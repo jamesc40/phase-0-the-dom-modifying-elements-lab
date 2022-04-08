690db750137e44aedb7df4bef4291ca1661804b6
James Coffman <jamesc4023@gmail.com>
2022-04-08 14:18:44 -0400

    commit tree 670fc4cd1ed3335640bc72386bb47221e1f6e4e2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// declare name variable
+const name = 'James';
+//remove main
+main.remove();
+//create newHeader variable assigned to newly created h1 element
+
+const newHeader = document.createElement('h1');
+//assign ID to victory
+newHeader.id= 'victory';
+//get fancy with text content
+newHeader.textContent = `${name} is the champion`;
+//throw it on the big screen
+document.body.append(newHeader);
